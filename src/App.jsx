3ab8ef000e0dbe4bc84b978df226d8b295608a33
SinghalSahab <prakharsinghal10a@gmail.com>
2024-12-01 02:26:53 +0530

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
       <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <section className='min-h-screen bg-blue-100'></section>
       </main>
     
   )
