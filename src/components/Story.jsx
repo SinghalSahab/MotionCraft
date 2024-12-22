78a212cbf10b3e6692f43b2ed15f579d53e2ee98
SinghalSahab <prakharsinghal10a@gmail.com>
2024-12-23 02:01:28 +0530

    contact and footer done

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,47 +1,49 @@
-import React, { useRef } from 'react'
+import gsap from "gsap";
+import { useRef } from "react";
+
+import Button from "./Button";
 import AnimatedTitle from "./AnimatedTitle";
-import gsap from 'gsap';
-import Button from './Button';
 
-const Story = () => {
-  const frameRef = useRef('null');
-  const handleMouseLeave = ()=>{
+const FloatingImage = () => {
+  const frameRef = useRef(null);
 
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
     const element = frameRef.current;
 
-    gsap.to(element,{
-      duration:0.3,
-      rotateX:0,
-      rotateY:0,
-      ease: 'power1.inOut',
-      
-     })
-  }
+    if (!element) return;
 
-  const handleMouseMove = (e)=>{
-     const {clientX, clientY} = e;
-     const element = frameRef.current;
+    const rect = element.getBoundingClientRect();
+    const xPos = clientX - rect.left;
+    const yPos = clientY - rect.top;
 
-     if(!element) return;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
 
-     const rect = element.getBoundingClientRect();
-     const x = clientX - rect.left;
-     const y = clientY - rect.top;
+    const rotateX = ((yPos - centerY) / centerY) * -10;
+    const rotateY = ((xPos - centerX) / centerX) * 10;
 
-     const centerX = rect.width/2;
-     const centerY = rect.height/2;
-
-     const rotateX = ((y - centerY) / centerY) * -10;
-     const rotateY = ((x - centerX) / centerX) * 10;
-
-     gsap.to(element,{
-      duration:0.3,
-      rotateX,rotateY,
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
       transformPerspective: 500,
-      ease: 'power1.inOut',
-      
-     })
-  }
+      ease: "power1.inOut",
+    });
+  };
+
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+
+    if (element) {
+      gsap.to(element, {
+        duration: 0.3,
+        rotateX: 0,
+        rotateY: 0,
+        ease: "power1.inOut",
+      });
+    }
+  };
   return (
     <section id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
       <div className='flex size-full flex-col items-center py-10 pb-24'>
@@ -112,4 +114,4 @@ const Story = () => {
   )
 }
 
-export default Story
+export default FloatingImage;
