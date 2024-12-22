778ac913a023181e91c4829ede65b0c843bf1bb9
SinghalSahab <prakharsinghal10a@gmail.com>
2024-12-23 01:32:13 +0530

    story done

---
+++ src/components/RoundedCorner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RoundedCorner = () => {
+  return (
+    <div>RoundedCorner</div>
+  )
+}
+
+export default RoundedCorner
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,10 +1,21 @@
 import React, { useRef } from 'react'
 import AnimatedTitle from "./AnimatedTitle";
+import gsap from 'gsap';
+import Button from './Button';
 
 const Story = () => {
   const frameRef = useRef('null');
   const handleMouseLeave = ()=>{
 
+    const element = frameRef.current;
+
+    gsap.to(element,{
+      duration:0.3,
+      rotateX:0,
+      rotateY:0,
+      ease: 'power1.inOut',
+      
+     })
   }
 
   const handleMouseMove = (e)=>{
@@ -22,6 +33,14 @@ const Story = () => {
 
      const rotateX = ((y - centerY) / centerY) * -10;
      const rotateY = ((x - centerX) / centerX) * 10;
+
+     gsap.to(element,{
+      duration:0.3,
+      rotateX,rotateY,
+      transformPerspective: 500,
+      ease: 'power1.inOut',
+      
+     })
   }
   return (
     <section id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
@@ -41,11 +60,53 @@ const Story = () => {
                      onMouseEnter={handleMouseLeave}
                      onMouseLeave={handleMouseLeave}
                      onMouseUp={handleMouseLeave}
-                     onMouseMove={handleMouseMove}/>
+                     onMouseMove={handleMouseMove}
+                     className="object-contain"/>
                   </div>
                 </div>
+                <svg
+              className="invisible absolute size-0"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <filter id="flt_tag">
+                  <feGaussianBlur
+                    in="SourceGraphic"
+                    stdDeviation="8"
+                    result="blur"
+                  />
+                  <feColorMatrix
+                    in="blur"
+                    mode="matrix"
+                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                    result="flt_tag"
+                  />
+                  <feComposite
+                    in="SourceGraphic"
+                    in2="flt_tag"
+                    operator="atop"
+                  />
+                </filter>
+              </defs>
+            </svg>
                </div>
             </div>
+
+            <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+
+            <Button
+              id="realm-btn"
+              title="discover prologue"
+              containerClass="mt-5"
+            />
+          </div>
+        </div>
       </div>
     </section>
   )
