5f54bf2b6dfd97d4e927d35857dbf238560dd9cb
SinghalSahab <prakharsinghal10a@gmail.com>
2026-05-25 02:40:12 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
-import { FaDiscord, FaTwitter, FaYoutube, FaMedium, FaInstagram } from "react-icons/fa";
+import { FaDiscord, FaTwitter, FaYoutube, FaMedium, FaInstagram, FaLinkedinIn, FaInstagramSquare } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
 
 const socialLinks = [
   { href: "https://discord.com", icon: <FaDiscord /> },
   { href: "https://twitter.com", icon: <FaTwitter /> },
   { href: "https://youtube.com", icon: <FaYoutube /> },
-  { href: "https://instagram.com", icon: <FaInstagram /> },
+  { href: "https://linkedin.com", icon: <FaLinkedinIn /> },
 ];
 
 const Footer = () => {
@@ -12,7 +13,7 @@ const Footer = () => {
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          ©Nova 2024. All rights reserved
+          ©Prakhar 2024. All rights reserved
         </p>
 
         <div className="flex justify-center gap-4  md:justify-start">
